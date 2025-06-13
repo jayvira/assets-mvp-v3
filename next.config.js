b99acef815ceb65f1a7f9edce9370a4eb5aa7afd
Mary Carnes <marycarnes@mary-qggw7f.bat-musical.ts.net>
2025-06-13 15:08:15 -0400

    my first deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/spring',
+  basePath: '/mary-prototype',
   images: {
     unoptimized: false,
   },
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getImagePath(path: string) {
-  return `/spring${path}`
+  return `/mary-prototype${path}`
 }
